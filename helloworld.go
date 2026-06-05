7174ea21be3d4621dab52a61f91b1d00be7164b6
Zen-107 <78131761+Zen-107@users.noreply.github.com>
2026-06-05 14:04:04 +0700

    เปลี่ยนชื่อ

---
+++ helloworld.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt" //แสดงผลออกทางหน้าจอ
+//ต้องเรียกใช้ package fmt นะ ไม่เรียก Go ลบออกเด้อ
+
+func main() {
+	//func ที่ Go จะมองเอาลำดับแรก เรียกง่ายๆคืออีนี่ทำงานก่อน
+	fmt.Println("Hello World")
+	fmt.Println("Golang")
+}
